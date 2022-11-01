4ac0b35d55c5d2558337e7aee3ae2bb887c196d4
Your Name <wasadmin@email.com>
2022-11-01 17:20:47 -0400

    Update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://10.108.107.32:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.98.157.113:8081/' })); 
+    app.use(proxy('/api', { target: 'http://10.111.82.245:8080/'}));
+    app.use(proxy('/account', { target: 'http://10.100.95.4:8081/' })); 
 	
 	/*
 	app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
